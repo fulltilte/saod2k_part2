09ddb12fbe18d745f8f71e49a983b53ba1912fed
Шведов Иван Алексеевич <ShIA.21@free.uni-dubna.ru>
2023-02-09 15:31:11 +0300

    new ex

---
+++ ex1_HelloWorld/ex1(HelloWorld).cpp
@@ -0,0 +1,11 @@
+﻿// ex1(HelloWorld).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
