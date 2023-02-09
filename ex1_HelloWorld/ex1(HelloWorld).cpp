bf0ea80e8d6522c9e3e4f9994ad278dfa1b8a0b5
Шведов Иван Алексеевич <ShIA.21@free.uni-dubna.ru>
2023-02-09 15:45:24 +0300

    update

--- ex1_HelloWorld/ex1(HelloWorld).cpp
+++ ex1_HelloWorld/ex1(HelloWorld).cpp
@@ -7,5 +7,13 @@ using namespace std;
 int main()
 {
     cout << "Hello, World!" << endl;
-    return 0;
+
+    int x, y;
+    cout << "x = ? "; cin >> x;
+    cout << "y = ? "; cin >> y;
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " / " << y << " = " << x / y << endl;
+    cout << x << " pow " << y << " = " << pow(x, y) << endl;
 }
