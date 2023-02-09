d9401e7bca9af01022ca21a52b3e92b0be54c92c
Шведов Иван Алексеевич <ShIA.21@free.uni-dubna.ru>
2023-02-09 16:04:04 +0300

    update

---
+++ ex2_Pointers/ex2(Array).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 3;
+    int *p = &x;
+    cout << x << ' ' << *p << ' ' << p << endl;
+
+    p++;
+    cout << p << endl;
+
+    cout << p - &x << endl;
+
+    typedef unsigned char byte;
+
+    byte* pb = (byte*)--p;
+    for (byte* pt = pb; pt - pb < sizeof(int); pt++)
+        cout << (int)*pt << ' ';
+
+    cout << endl;
+}
