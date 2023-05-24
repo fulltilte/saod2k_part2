8f7a8d91659746f6655f03ef2da8dfd653dbe636
Ivan Shvedov <48998946+fulltilte@users.noreply.github.com>
2023-05-25 00:59:38 +0300

    Update Archive.cpp

--- additional_ex5/Archive.cpp
+++ additional_ex5/Archive.cpp
@@ -1,105 +1,88 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
-#include <unordered_map>
+#include <map>
 #include <string>
+#include <vector>
 
-const int MAX_DICT_SIZE = 128;
+using namespace std;
 
-std::string compressLZ78(const std::string& input) {
-    std::unordered_map<std::string, int> dictionary;
-    std::string compressed;
+void compress(const string& input_file, const string& output_file) {
+    ifstream in(input_file, ios::binary);
+    ofstream out(output_file, ios::binary);
 
-    std::string currentMatch;
-    int nextCode = 1;
+    map<string, int> dictionary;
+    vector<int> encoded_data;
 
-    for (char c : input) {
-        currentMatch += c;
+    int code = 0;
+    string w;
+    char c;
 
-        // Проверяем, содержится ли текущая последовательность в словаре
-        if (dictionary.count(currentMatch) == 0) {
-            // Если последовательность отсутствует, добавляем ее в словарь
-            dictionary[currentMatch] = nextCode++;
-            // Записываем код предыдущей последовательности в сжатый вывод
-            compressed += std::to_string(dictionary[currentMatch.substr(0, currentMatch.length() - 1)]);
-
-            // Если словарь достиг максимального размера, очищаем его
-            if (nextCode >= MAX_DICT_SIZE) {
-                dictionary.clear();
-                nextCode = 1;
-            }
-
-            currentMatch = "";
+    while (in.get(c)) {
+        string wc = w + c;
+        if (dictionary.count(wc)) {
+            w = wc;
+        }
+        else {
+            encoded_data.push_back(dictionary[w]);
+            dictionary[wc] = code++;
+            w = string(1, c);
         }
     }
 
-    // Записываем код последней последовательности в сжатый вывод
-    if (!currentMatch.empty()) {
-        compressed += std::to_string(dictionary[currentMatch]);
+    if (!w.empty()) {
+        encoded_data.push_back(dictionary[w]);
     }
 
-    return compressed;
+    for (int code : encoded_data) {
+        out.write(reinterpret_cast<const char*>(&code), sizeof(code));
+    }
 }
 
-std::string decompressLZ78(const std::string& compressed) {
-    std::unordered_map<int, std::string> dictionary;
-    std::string decompressed;
+void decompress(const string& input_file, const string& output_file) {
+    ifstream in(input_file, ios::binary);
+    ofstream out(output_file, ios::binary);
 
-    std::string currentMatch;
-    int nextCode = 1;
+    map<int, string> dictionary;
+    vector<int> encoded_data;
 
-    for (char c : compressed) {
-        currentMatch += c;
-
-        // Проверяем, является ли текущий символ числом
-        if (isdigit(c)) {
-            int code = std::stoi(currentMatch);
-            if (code == 0) {
-                // Новая последовательность, добавляем символ в результат
-                decompressed += currentMatch.back();
-                dictionary[nextCode++] = std::string(1, currentMatch.back());
-            }
-            else {
-                // Декодируем последовательность из словаря и добавляем в результат
-                std::string sequence = dictionary[code] + currentMatch.back();
-                decompressed += sequence;
-                dictionary[nextCode++] = sequence;
-            }
-
-            // Очищаем текущую последовательность
-            currentMatch = "";
-        }
+    int code;
+    while (in.read(reinterpret_cast<char*>(&code), sizeof(code))) {
+        encoded_data.push_back(code);
     }
 
-    return decompressed;
+    int dict_size = 128;
+    for (int i = 0; i < dict_size; i++) {
+        dictionary[i] = string(1, i);
+    }
+
+    string w = dictionary[encoded_data[0]];
+    out << w;
+
+    for (int i = 1; i < encoded_data.size(); i++) {
+        int code = encoded_data[i];
+        string entry;
+        if (dictionary.count(code)) {
+            entry = dictionary[code];
+        }
+        else if (code == dict_size) {
+            entry = w + w[0];
+        }
+        else {
+            throw "Bad compressed code";
+        }
+        out << entry;
+        dictionary[dict_size++] = w + entry[0];
+        w = entry;
+    }
 }
 
 int main() {
-    std::locale::global(std::locale("Russian"));
+    string input_file = "text.txt";
+    string archive_file = "archive.bin";
+    string output_file = "output.txt";
 
-    std::ifstream inputFile("text.txt"); // Открываем файл для чтения входных данных
-    std::ofstream compressedFile("compressed.txt"); // Открываем файл для записи сжатого вывода
-    std::ofstream decompressedFile("decompressed.txt"); // Открываем файл для записи декомпрессированного вывода
-
-    if (!inputFile || !compressedFile || !decompressedFile) {
-        std::cout << "Ошибка открытия файлов." << std::endl;
-        return 1;
-    }
-
-    std::string input;
-    std::getline(inputFile, input); // Читаем строку из файла
-
-    std::string compressed = compressLZ78(input);
-    compressedFile << compressed; // Записываем сжатый вывод в файл
-
-    std::string decompressed = decompressLZ78(compressed);
-    decompressedFile << decompressed; // Записываем декомпрессированный вывод в файл
-
-    std::cout << "Сжатый вывод записан в файл 'compressed.txt'." << std::endl;
-    std::cout << "Декомпрессированный вывод записан в файл 'decompressed.txt'." << std::endl;
-
-    inputFile.close(); // Закрываем файлы
-    compressedFile.close();
-    decompressedFile.close();
+    compress(input_file, archive_file);
+    decompress(archive_file, output_file);
 
     return 0;
 }
