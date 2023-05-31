b27bc914f4c60aa482ad9c72147caf6b41801f0d
Ivan Shvedov <48998946+fulltilte@users.noreply.github.com>
2023-06-01 01:39:55 +0300

    Update Program.cs

--- ex11/Program.cs
+++ ex11/Program.cs
@@ -1,11 +1,8 @@
-﻿using System;
+using System;
 using System.Text.RegularExpressions;
 using System.IO;
 using System.Xml;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using System.Xml.Linq;
+
 
 namespace RegExpLab
 {
@@ -13,168 +10,144 @@ namespace RegExpLab
     {
         static void Main(string[] args)
         {
-            var str = " фыидо, fjbld33- 2олт?";
-            // task 1.a
-            var re = new Regex(@"."); // ищем любой символ
-            Mathing(str, re,"a");
-            // task 1.b
-            re = new Regex(@"\w+"); // ищем слова
-            Mathing(str, re,"b");
-            // task 1.c
-            re = new Regex(@"\b[0-9]+\w+\b"); // ищем слова начинающиеся с цифры
-            Mathing(str, re, "c");
-            // task 1.d
-            str = "фыидо, fjbld33- 2олт 2p2 f8f73?";
-            re = new Regex(@"\b(\w+\d+)|(\d+\w+)|(\w+\d+\w+)\b"); // ищем слова в котрых есть хоть одна цифрв
-            Mathing(str, re, "d");
-            // task 1.e
-            re = new Regex(@"\w+3\b"); // ищем слова с тройкой на конце
-            Mathing(str, re, "e");
-            // task 1.f
-            re = new Regex(@"(\w)\1"); // ищем два раза повторившийся символ слова 
-            Mathing(str, re, "f");
-            // task 1.g
-            re = new Regex(@"\b\w+(\w)\1|(\w)\2\w+\b"); // поиск слов с сдвоенными символами
-            Mathing(str, re, "g");
-            // task 1.h
-            str = " фыидо, fjbld33- 2олт?";
-            re = new Regex(@"(\W*)(\w+)(\W+)(\w+)(\W+)(\w+)(\W*)"); // делим текст на семь частей
-            Console.WriteLine(re.Replace(str, "$1$4$3$2$5$6$7")); // меняем вторую и четретую часть местами fjbld33- <=> фыидо
-            // task 1.i
-            re = new Regex(@"\b[0-9a-zA-Z]+\b");//слова только с латинскими буквами и цифрами. 
-            Mathing(str, re, "i");
-            // task 1.j
-            re = new Regex(@"\b[a-zA-Z]\w+|\w+[a-zA-Z]\w*\b"); // находит все слова в котрый есть хотя бы одна латинская буква
-            Mathing(str, re, "j");
-            // task 1.k
-            string orig;
+            string str = " фыидо, fjbld33- 2олт?";
+            Console.WriteLine(str);
+            Console.WriteLine("");
+
+            //Regex re = new Regex(@".");               
+            //a - по символам  (@"."означает поиск любого символа)
+
+            //Regex re = new Regex(@"\w+");             
+            //b - по словам  (@"\w+" означает поиск одного или более символов)
+
+            //Regex re = new Regex(@"\b[0-9]+\w+\b");   
+            //c - последнее слово  (в регулярном выражении указано, что нужно найти слово, которое начинается с одной или более цифр (\b[0-9]+), за которыми следует
+            //одно или более буквенных символов (\w+), и заканчивается границей слова (\b))
+
+            //Regex re = new Regex(@"\w*[0-9]+\w*");    
+            //d - хотя бы 1 цыфра
+
+            //Regex re = new Regex(@"\w+3\b");          
+            //e - 2e слово  (в @"\w+3\b" соответствует любому символу слова один или несколько раз (\w+), за которым сразу следует цыфра 3 и которая находится в конце слова (\b)
+
+            //Regex re = new Regex(@"(\w)\1");          
+            //f - 33  (В @"(\w)\1" круглые скобки создают группу, которая соответствует любому символу слова (\w) и сохраняет его в первой группе.
+            //\1 - это обратная ссылка, которая соответствует тому же символу, который был подобран в первой группе.
+
+            //Regex re = new Regex(@"\w*(\w)\1\w*");    
+            //g - слова со сдвоенными символами
+
+            //Regex re = new Regex(@"(\W*)(\w+)(\W+)(\w+)(\W+)(\w+)(\W*)"); 
+            //h  
+            //(\W*): группа, которая соответствует нулю или более несловесным символам (\W).
+            //Цель этой группы - захватить все символы, не являющиеся словами, которые появляются перед первым словом в строке, и сохранить их положение в выходной строке.
+
+            //(\w+): группа, которая соответствует одному или нескольким символам слова (\w).
+            //Цель этой группы - захватить первое слово в строке и сохранить его позицию в выходной строке
+
+            //(\W+): Это группа захвата, которая соответствует одному или нескольким символам, не являющимся словом (\W). каждой парой слов в строке, и сохранить их положение в выходной строке.
+
+            //(\w+): Цель этой группы - захватить второе слово в строке и сохранить его позицию в выходной строке.
+
+            //(\W+): Цель этой группы - захватить символы, не являющиеся словами, которые появляются между вторым и третьим словами в строке, и сохранить их положение в выходной строке.
+
+            //(\w+): Цель этой группы - захватить третье слово в строке и сохранить его позицию в выходной строке.
+
+            //(\W*): Цель этой группы - захватить все символы, не являющиеся словами, которые появляются после последнего слова в строке, и сохранить их положение в выходной строке.
+
+            //Console.WriteLine(re.Replace(str, "$1$4$3$2$5$6$7"));            //Строка замены "$1$4$3$2$5$6$7" использует захваченные группы для перестановки слов
+            //и несловесных символов в строке таким образом, чтобы в выходной строке второе и третье слова
+            //поменялись местами, а позиции несловесных символов были сохранены. $1, $2, $3, $4, $5, $6, и $7
+            //- это заполнители, которые ссылаются на текст, захваченный соответствующими группами захвата в регулярном выражении.
+
+            //Regex re = new Regex(@"\b([a-zA-Z0-9]+)\b");                  
+            //i - только латинские буквы и цыфры
+
+            //Regex re = new Regex(@"\w*[a-zA]+\w+");                       
+            //j - хотя бы 1 латинская буква
+
+            string orig;                                                  //k
             try
             {
-                using (StreamReader sr = new StreamReader("Dictionary.xml"))
+                using (StreamReader sr = new StreamReader("CreateDB.sql"))
                 {
                     orig = sr.ReadToEnd();
-                    Console.WriteLine(orig.Length);
+                    //Console.WriteLine(orig.Length);
                 }
 
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Ошибка чтения файла: \"Dictionary.xml\"\n{0}", ex);
+                Console.WriteLine("Ошибка чтения файла: \" CreateDB.sql\"\n{0}", ex);
                 return;
             }
-            
-            Task_l(orig, "l");
-            // task 1.n
-            re = new Regex(@"(?<tag1>[\w-]+)>\s*[А-Я]\s*</\k<tag1>");
-            var trans = Task_n(orig, "n");
-            var trans2 = Task_m(orig, "m");
-            // check 1
-            trans = Task_n(trans2, "n");
 
-            var trans3 = Task_o(trans2, "o");
-            var trans4 = Task_q(trans2, "q");
+            //Regex re = new Regex(@"[(](?![0-9,]+)[^.]+[)]");              //l
 
-            //check 2 
-            trans3 = Task_o(trans4, "o");
+            Regex re = new Regex(@"/((?!\d+(?:\s*,\s*\d+)*)[^)]+\))");
+
+            //MatchCollection mc = re.Matches(orig);
+            //foreach (var mm in mc)
+            //    Console.WriteLine($"\n{mm}\n");
+
+            //Console.WriteLine(mc.Count);
+
+            //MatchCollection mc = re.Matches(str);
+            //foreach (var m in mc)
+            //    Console.WriteLine(m);
+
+            string CreateDB = orig;
+            {
+                CreateDB = Regex.Replace(CreateDB, @"(GO)|(SET.+)", "");
+
+                CreateDB = Regex.Replace(CreateDB, @"(CONSTRAINT\s\[.+\]\sPRIMARY\sKEY\sCLUSTERED\s)", "PRIMARY KEY");
+                
+                CreateDB = Regex.Replace(CreateDB, @"(\[.+\]\.)|([\[\]])|()", "");
+
+                CreateDB = Regex.Replace(CreateDB, @"(WITH \(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON\) ON .+)", "");
+                 
+                CreateDB = Regex.Replace(CreateDB, @"(IDENTITY\([0-9].[0-9]\))", "AUTO_INCREMENT");
+
+                CreateDB = Regex.Replace(CreateDB, @"(timestamp NOT NULL)", "timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP");
+
+                CreateDB = Regex.Replace(CreateDB, @"(PRIMARY\sKEY\sNONCLUSTERED\s)", "PRIMARY KEY");
+                CreateDB = Regex.Replace(CreateDB, @"(UNIQUE\sNONCLUSTERED\s)", "UNIQUE KEY");
+
+                CreateDB = Regex.Replace(CreateDB, @"\)\nPRIMARY\sKEY", "),\nPRIMARY KEY");
+                CreateDB = Regex.Replace(CreateDB, @"\)\nUNIQUE\sKEY", "),\nUNIQUE KEY");
+                 
+                CreateDB = Regex.Replace(CreateDB, @"( ON PRIMARY TEXTIMAGE_ON PRIMARY)", ";");
+                CreateDB = Regex.Replace(CreateDB, @"( ON PRIMARY)", ";");
+                
+                CreateDB = Regex.Replace(CreateDB, @"(uniqueidentifier)", "char(32)");
+                
+                CreateDB = Regex.Replace(CreateDB, @"(xml)|(ntext)|(nvarchar\(max\))", "text");
+
+                CreateDB = Regex.Replace(CreateDB, @"(image)|(varbinary\(max\))", "BLOB");
+
+                CreateDB = Regex.Replace(CreateDB, @"USE LockeyDB", "");//удаление той самой первой ошибки
+            }
 
             try
             {
-                using (StreamWriter sw = new StreamWriter("test.xml"))
+                using (StreamWriter sw = new StreamWriter("CreateDB_MySQL.sql"))
                 {
-                    sw.Write(trans4);
+                    sw.Write(CreateDB);
+                    sw.Close();
                 }
-                Console.WriteLine("write complete");
+                Console.WriteLine("Class");
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Ошибка сохранений в файл: \"{0}\"\n{1}", "test.xml", ex);
+                Console.WriteLine("Ошибка сохранений в файл: \"{0}\"\n{1}", "CreateDB_MySQL.sql", ex);
                 return;
             }
-            try
-            {
-                XmlDocument doc = new XmlDocument();
-                doc.LoadXml(trans4);
-                Console.WriteLine("xml load succes");
-            }
-            catch
-            {
-                Console.WriteLine("something wrong!(");
-            }
-            Console.Read();
-        }
-        private static void Mathing(string str, Regex re, string name = "test", bool isStrShow = true)
-        {
-            Console.WriteLine(" \n\rstart mathing " + name);
-            if(isStrShow) Console.WriteLine("Text : "+str);
-            MatchCollection mc = re.Matches(str);
-            foreach (var m in mc)
-                Console.WriteLine(m);
-            Console.WriteLine("end mathing " +name);
-        }
-        private static void Task_l(string str, string name = "test") 
-        {
-            var re = new Regex(@"<(?<tag1>[\w-]+)>[^><]*</\k<tag1>>");
-            Console.WriteLine(" \n\rstart mathing " + name);
-            MatchCollection mc = re.Matches(str);
-            var s = new StringBuilder();
-            foreach (var m in mc)
-                s.Append(m.ToString());
-            re = new Regex(@"[\w-]+(?=>)");
-            mc = re.Matches(s.ToString());
-            var ls = new List<string>();
-            foreach (var m in mc)
-                ls.Add(m.ToString()); 
-            ls = ls.Distinct().ToList();
-            foreach (var m in ls)
-                Console.WriteLine(m);
-            
-            Console.WriteLine("end mathing " + name);
-        }
-        private static string Task_n(string str, string name = "test")
-        {
-            var  re = new Regex(@"((?<tag2>[\w-]+)?<(?<tag1>(?!italic)[\w-]+)>\s*(?<l>[А-Я])\s*</\k<tag1>>(k\<tag2>)?)");
-            Console.WriteLine(" \n\rstart mathing " + name);
-            MatchCollection mc = re.Matches(str);
-            var sb = new StringBuilder();
-            foreach (var m in mc)
-            {
-                sb.Append(m.ToString());
-                Console.WriteLine(m);
-            }
-            Console.WriteLine("end mathing " + name);
-            return sb.ToString();
-        }
-        private static string Task_m(string str, string name = "test")
-        {
-            var re = new Regex(@"((?<tag2>[\w-]+)?<(?<tag1>(?!italic)[\w-]+)>\s*(?<l>[А-Я])\s*</\k<tag1>>(k\<tag2>)?)");
-            Console.WriteLine(" \n\rstart mathing " + name);
-            MatchCollection mc = re.Matches(str);
-            var s = re.Replace(str, "<letter><bold>${l}</bold></letter>\n\r");
-            Console.WriteLine("end mathing " + name);
-            return s;
-        }
-        private static string Task_o(string str, string name = "test")
-        {
-            var re = new Regex(@"</bold></letter>\s*(?<useless><(?<tag1>(?!(main-word|mainword))[\w-]+)>\s*</\k<tag1>>)");
-            Console.WriteLine(" \n\rstart mathing " + name);
-            MatchCollection mc = re.Matches(str);
-            var sb = new StringBuilder();
-            foreach (var m in mc)
-            {
-                sb.Append(m.ToString());
-                Console.WriteLine(m);
-            }
-            Console.WriteLine("end mathing " + name);
-            return sb.ToString();
-        }
-        private static string Task_q(string str, string name = "test")
-        {
-            var re = new Regex(@"(?<uses></bold></letter>\s*)(?<useless><(?<tag1>(?!(main-word|mainword))[\w-]+)>\s*</\k<tag1>>)");
-            Console.WriteLine(" \n\rstart mathing " + name);
-            MatchCollection mc = re.Matches(str);
-            var s = re.Replace(str, "${uses}");
-            Console.WriteLine("end mathing " + name);
-            return s;
+
+            //Console.WriteLine(CreateDB);
+
+            //MatchCollection mc2 = re.Matches(CreateDB);
+            //foreach (var mm in mc2)
+            //    Console.WriteLine($"\n{mm}\n");
         }
     }
 }
